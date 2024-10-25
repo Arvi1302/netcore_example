3582de0ff4edbc8e1ef2329c33d22f680d51a25d
toshiossada <toshiossada@gmail.com>
2024-10-25 11:33:01 -0300

    ibit

---
+++ android/app/src/main/kotlin/br/com/farmaciasapp/netcore_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.com.farmaciasapp.netcore_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/src/app_widget.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:smartech_nudges/tracker/route_obersver.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Modular.setInitialRoute('/splash');
+    Modular.setObservers([PxNavigationObserver()]);
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      title: 'Farmácias APP',
+      routerConfig: Modular.routerConfig,
+    );
+  }
+}
